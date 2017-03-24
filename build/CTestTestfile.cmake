8bb5199b6769ffc9582348174e0b6499a2493741
Nieis <monika_1393@tlen.pl>
2017-03-24 12:18:39 +0100

    init commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: D:/StudiaMGR/semestr 3/PWP/lab2/OpenIGTLink
+# Build directory: D:/StudiaMGR/semestr 3/PWP/lab2/OpenIGTLink/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("Source")
+subdirs("Tools")
+subdirs("Examples")
+subdirs("Testing")
